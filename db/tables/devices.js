b4837dcd2dd22df3a39907bfd081d8cf0bf3daea
kunjinx <1109369177@qq.com>
2018-08-28 19:07:48 +0800

    Merge branch 'dev' of https://github.com/kunjinx/NATP into dev
    # Conflicts:
    #	db/api.js
    #	db/index.js
    #	db/setup.js

--- db/tables/devices.js
+++ db/tables/devices.js
@@ -15,7 +15,7 @@ module.exports = {
         ready: Sequelize.BOOLEAN,
         remoteConnect: Sequelize.BOOLEAN,
         remoteConnectUrl: Sequelize.STRING,
-        serial: { type: Sequelize.STRING, primaryKey: true },
+        serial: {type: Sequelize.STRING, primaryKey: true},
         status: Sequelize.INTEGER,
         statusChangedAt: Sequelize.DATE,
         usage: Sequelize.STRING,
@@ -35,85 +35,89 @@ module.exports = {
         notes: Sequelize.STRING
     },
     network: {
-        connected:Sequelize.BOOLEAN,
-        failover:Sequelize.BOOLEAN,
-        roaming:Sequelize.BOOLEAN,
-        subtype:Sequelize.STRING,
-        type:Sequelize.STRING,
-        serial:{type:Sequelize.STRING,primaryKey:true}
+        connected: Sequelize.BOOLEAN,
+        failover: Sequelize.BOOLEAN,
+        roaming: Sequelize.BOOLEAN,
+        subtype: Sequelize.STRING,
+        type: Sequelize.STRING,
+        serial: {type: Sequelize.STRING, primaryKey: true}
     },
     battery: {
-        health:Sequelize.STRING,
-        level:Sequelize.INTEGER,
-        scale:Sequelize.INTEGER,
-        source:Sequelize.STRING,
-        status:Sequelize.STRING,
-        temp:Sequelize.FLOAT,
-        voltage:Sequelize.FLOAT,
-        serial:{type:Sequelize.STRING,primaryKey:true}
+        health: Sequelize.STRING,
+        level: Sequelize.INTEGER,
+        scale: Sequelize.INTEGER,
+        source: Sequelize.STRING,
+        status: Sequelize.STRING,
+        temp: Sequelize.FLOAT,
+        voltage: Sequelize.FLOAT,
+        serial: {type: Sequelize.STRING, primaryKey: true}
     },
     owner: {
-        value:Sequelize.STRING,
-        email:Sequelize.STRING,
-        group:Sequelize.STRING,
-        name:Sequelize.STRING,
-        serial:{type:Sequelize.STRING,primaryKey:true}
+        value: Sequelize.STRING,
+        email: Sequelize.STRING,
+        group: Sequelize.STRING,
+        name: Sequelize.STRING,
+        serial: {type: Sequelize.STRING, primaryKey: true}
     },
     provider: {
-        channel:Sequelize.STRING,
-        name:Sequelize.STRING,
-        serial:{type:Sequelize.STRING,primaryKey:true}
+        channel: Sequelize.STRING,
+        name: Sequelize.STRING,
+        serial: {type: Sequelize.STRING, primaryKey: true}
     },
     reverseForwards: {
-        forwards: {type:Sequelize.STRING,primaryKey:true},
-        serial:{type:Sequelize.STRING,primaryKey:true}
+        forwards: {type: Sequelize.STRING, primaryKey: true},
+        serial: {type: Sequelize.STRING, primaryKey: true}
     },
-    browser:{
-        selected:Sequelize.BOOLEAN,
-        serial:{type:Sequelize.STRING,primaryKey:true}
+    browser: {
+        selected: Sequelize.BOOLEAN,
+        serial: {type: Sequelize.STRING, primaryKey: true}
     },
     apps: {
-        id:{type:Sequelize.STRING,primaryKey:true},
-        name:Sequelize.STRING,
-        selected:Sequelize.BOOLEAN,
-        system:Sequelize.BOOLEAN,
-        type:Sequelize.STRING,
-        serial:{type:Sequelize.STRING,primaryKey:true}
+        id: {type: Sequelize.STRING, primaryKey: true},
+        name: Sequelize.STRING,
+        selected: Sequelize.BOOLEAN,
+        system: Sequelize.BOOLEAN,
+        type: Sequelize.STRING,
+        serial: {type: Sequelize.STRING, primaryKey: true}
     },
     display: {
-        density:Sequelize.FLOAT,
-        fps:Sequelize.FLOAT,
-        height:Sequelize.INTEGER,
-        id:Sequelize.INTEGER,
-        rotation:Sequelize.INTEGER,
-        secure:Sequelize.INTEGER,
-        size:Sequelize.FLOAT,
-        url:Sequelize.STRING,
-        width:Sequelize.INTEGER,
-        xdpi:Sequelize.INTEGER,
-        ydpi:Sequelize.INTEGER,
-        serial:{type:Sequelize.STRING,primaryKey:true}
+        density: Sequelize.FLOAT,
+        fps: Sequelize.FLOAT,
+        height: Sequelize.INTEGER,
+        id: Sequelize.INTEGER,
+        rotation: Sequelize.INTEGER,
+        secure: Sequelize.INTEGER,
+        size: Sequelize.FLOAT,
+        url: Sequelize.STRING,
+        width: Sequelize.INTEGER,
+        xdpi: Sequelize.INTEGER,
+        ydpi: Sequelize.INTEGER,
+        serial: {type: Sequelize.STRING, primaryKey: true}
     },
     phone: {
-        iccid:Sequelize.STRING,
-        imei:Sequelize.STRING,
-        imsi:Sequelize.STRING,
-        network:Sequelize.STRING,
-        phoneNumber:Sequelize.STRING,
-        serial:{type:Sequelize.STRING,primaryKey:true}
+        iccid: Sequelize.STRING,
+        imei: Sequelize.STRING,
+        imsi: Sequelize.STRING,
+        network: Sequelize.STRING,
+        phoneNumber: Sequelize.STRING,
+        serial: {type: Sequelize.STRING, primaryKey: true}
     },
-        
+
 
     association: (models) => {
-        models.devices.hasOne(models.provider, { foreignKey: 'serial', as: 'provider', onDelete: 'CASCADE' });
-        models.devices.hasOne(models.battery, { foreignKey: 'serial', as: 'battery', onDelete: 'CASCADE' });
-        models.devices.hasOne(models.browser, { foreignKey: 'serial', as: 'browser', onDelete: 'CASCADE' });
-        models.devices.hasOne(models.display, { foreignKey: 'serial', as: 'display', onDelete: 'CASCADE' });
-        models.devices.hasOne(models.network, { foreignKey: 'serial', as: 'network', onDelete: 'CASCADE' });
-        models.devices.hasOne(models.phone, { foreignKey: 'serial', as: 'phone', onDelete: 'CASCADE' });
-        models.devices.hasOne(models.owner, { foreignKey: 'serial', as: 'owner', onDelete: 'CASCADE' });
+        models.devices.hasOne(models.provider, {foreignKey: 'serial', as: 'provider', onDelete: 'CASCADE'});
+        models.devices.hasOne(models.battery, {foreignKey: 'serial', as: 'battery', onDelete: 'CASCADE'});
+        models.devices.hasOne(models.browser, {foreignKey: 'serial', as: 'browser', onDelete: 'CASCADE'});
+        models.devices.hasOne(models.display, {foreignKey: 'serial', as: 'display', onDelete: 'CASCADE'});
+        models.devices.hasOne(models.network, {foreignKey: 'serial', as: 'network', onDelete: 'CASCADE'});
+        models.devices.hasOne(models.phone, {foreignKey: 'serial', as: 'phone', onDelete: 'CASCADE'});
+        models.devices.hasOne(models.owner, {foreignKey: 'serial', as: 'owner', onDelete: 'CASCADE'});
 
-        models.devices.hasMany(models.reverseForwards, { foreignKey: 'serial', as: 'reverseForwards', onDelete: 'CASCADE' });
-        models.browser.hasMany(models.apps, { foreignKey: 'serial', as: 'apps', onDelete: 'CASCADE' });
+        models.devices.hasMany(models.reverseForwards, {
+            foreignKey: 'serial',
+            as: 'reverseForwards',
+            onDelete: 'CASCADE'
+        });
+        models.browser.hasMany(models.apps, {foreignKey: 'serial', as: 'apps', onDelete: 'CASCADE'});
     }
 }
--- db/tables/users.js
+++ db/tables/users.js
@@ -10,7 +10,7 @@ module.exports = {
 
     users: {
         createdAt: Sequelize.DATE,
-        email: { type: Sequelize.STRING, primaryKey: true },
+        email: {type: Sequelize.STRING, primaryKey: true},
         group: Sequelize.STRING,
         ip: Sequelize.STRING,
         lastLoggedInAt: Sequelize.DATE,
@@ -18,20 +18,20 @@ module.exports = {
     },
 
     adbKeys: {
-        fingerprint: { type: Sequelize.STRING, primaryKey: true },
+        fingerprint: {type: Sequelize.STRING, primaryKey: true},
         title: Sequelize.STRING,
-        email: { type: Sequelize.STRING, primaryKey: true }        
+        email: {type: Sequelize.STRING, primaryKey: true}
     }
     ,
 
     forwards: {
-        forwards: { type: Sequelize.STRING, primaryKey: true },
-        email: { type: Sequelize.STRING, primaryKey: true } 
+        forwards: {type: Sequelize.STRING, primaryKey: true},
+        email: {type: Sequelize.STRING, primaryKey: true}
     }
     ,
 
     settings: {
-        email: { type: Sequelize.STRING, primaryKey: true },
+        email: {type: Sequelize.STRING, primaryKey: true},
         lastUsedDevice: Sequelize.STRING,
         selectedLanguage: Sequelize.STRING
     }
@@ -40,45 +40,53 @@ module.exports = {
     deviceListActiveTabs: {
         details: Sequelize.BOOLEAN,
         icons: Sequelize.BOOLEAN,
-        email: { type: Sequelize.STRING, primaryKey: true }
+        email: {type: Sequelize.STRING, primaryKey: true}
     }
     ,
 
     deviceListColumns: {
-        name: { type: Sequelize.STRING, primaryKey: true },
+        name: {type: Sequelize.STRING, primaryKey: true},
         selected: Sequelize.BOOLEAN,
-        email: { type: Sequelize.STRING, primaryKey: true }
+        email: {type: Sequelize.STRING, primaryKey: true}
     }
     ,
 
     deviceListSort: {
-        email: { type: Sequelize.STRING, primaryKey: true }
+        email: {type: Sequelize.STRING, primaryKey: true}
     }
     ,
 
     fixed: {
-        name: { type: Sequelize.STRING, primaryKey: true }, 
+        name: {type: Sequelize.STRING, primaryKey: true},
         order: Sequelize.STRING,
-        email: { type: Sequelize.STRING, primaryKey: true }
+        email: {type: Sequelize.STRING, primaryKey: true}
     }
     ,
 
     user: {
-        name: { type: Sequelize.STRING, primaryKey: true }, 
+        name: {type: Sequelize.STRING, primaryKey: true},
         order: Sequelize.STRING,
-        email: { type: Sequelize.STRING, primaryKey: true }
+        email: {type: Sequelize.STRING, primaryKey: true}
     }
     ,
 
     association: (models) => {
-        models.users.hasOne(models.settings, { foreignKey: 'email', as: 'settings' ,onDelete:'CASCADE'});
-        models.users.hasMany(models.adbKeys, { foreignKey: 'email', as: 'adbKeys',onDelete:'CASCADE' });
-        models.users.hasMany(models.forwards, { foreignKey: 'email', as: 'forwards',onDelete:'CASCADE' });
-        models.settings.hasOne(models.deviceListActiveTabs, { foreignKey:'email',as: 'deviceListActiveTabs',onDelete:'CASCADE' });
-        models.settings.hasOne(models.deviceListSort, { foreignKey:'email',as: 'deviceListSort',onDelete:'CASCADE' });
+        models.users.hasOne(models.settings, {foreignKey: 'email', as: 'settings', onDelete: 'CASCADE'});
+        models.users.hasMany(models.adbKeys, {foreignKey: 'email', as: 'adbKeys', onDelete: 'CASCADE'});
+        models.users.hasMany(models.forwards, {foreignKey: 'email', as: 'forwards', onDelete: 'CASCADE'});
+        models.settings.hasOne(models.deviceListActiveTabs, {
+            foreignKey: 'email',
+            as: 'deviceListActiveTabs',
+            onDelete: 'CASCADE'
+        });
+        models.settings.hasOne(models.deviceListSort, {foreignKey: 'email', as: 'deviceListSort', onDelete: 'CASCADE'});
 
-        models.settings.hasMany(models.deviceListColumns, { foreignKey:'email',as: 'deviceListColumns',onDelete:'CASCADE' });
-        models.deviceListSort.hasMany(models.fixed, { foreignKey:'email',as: 'fixed',onDelete:'CASCADE' });
-        models.deviceListSort.hasMany(models.user, { foreignKey:'email',as: 'user',onDelete:'CASCADE' });
+        models.settings.hasMany(models.deviceListColumns, {
+            foreignKey: 'email',
+            as: 'deviceListColumns',
+            onDelete: 'CASCADE'
+        });
+        models.deviceListSort.hasMany(models.fixed, {foreignKey: 'email', as: 'fixed', onDelete: 'CASCADE'});
+        models.deviceListSort.hasMany(models.user, {foreignKey: 'email', as: 'user', onDelete: 'CASCADE'});
     }
 }
